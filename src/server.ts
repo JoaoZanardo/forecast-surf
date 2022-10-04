ca1a4ba5ad7fba335d6c3b1e50e58b33463e5fa5
João Zanardo <joaozzz0105@gmail.com>
2022-10-04 18:05:11 -0300

    mongodb setup

--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,17 @@ import { Server } from '@overnightjs/core';
 import bodyParser from 'body-parser';
 import { Application } from 'express';
 import { ForecastController } from './controllers/forecast';
-import './util/module-alias';
+import * as database from '@src/database';
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
     super();
   }
 
-  init(): void {
+  async init(): Promise<void> {
     this.setupExpress();
     this.setupControllers();
+    await this.databaseSetup();
   }
 
   getApp(): Application {
@@ -25,5 +26,13 @@ export class SetupServer extends Server {
   private setupControllers(): void {
     const forecastController = new ForecastController();
     this.addControllers(forecastController);
+  }
+
+  private async databaseSetup(): Promise<void> {
+    await database.connect();
+  }
+
+  async close(): Promise<void> {
+    await database.close();
   }
 }
