655f162eb8b693d707e875b125db730c7c364096
João Zanardo <joaozzz0105@gmail.com>
2022-10-06 21:13:51 -0300

    beach model

--- src/controllers/beach.ts
+++ src/controllers/beach.ts
@@ -1,10 +1,21 @@
 import { Controller, Post } from "@overnightjs/core";
+import { Beach } from "@src/models/beach";
 import { Request, Response } from "express";
+import mongoose from "mongoose";
 
 @Controller('beaches')
 export class BeachesController {
     @Post('')
     async create(req: Request, res: Response): Promise<void> {
-        res.status(201).send({ ...req.body, id: 'FAKE-ID' });
+        try {
+            const beach = new Beach(req.body);
+            const result = await beach.save();
+            res.status(201).send(result);
+        } catch (error) {
+            const err = error as Error;
+            if (err instanceof mongoose.Error) {
+                res.status(422).send({ error: err.message });
+            }
+        }
     }
 }
--- test/functional/beaches.test.ts
+++ test/functional/beaches.test.ts
@@ -1,4 +1,8 @@
+import { Beach } from "@src/models/beach";
+
 describe('Beach functional tests', () => {
+    beforeAll(async() => await Beach.deleteMany({}));
+
     describe('When creating a beach', () => {
         it ('should create a beach with success', async () => {
             const beachBody = {
@@ -12,5 +16,25 @@ describe('Beach functional tests', () => {
             expect(response.status).toEqual(201);
             expect(response.body).toEqual(expect.objectContaining(beachBody));
         });
+
+        it('should return 422 when there is a validation error', async () => {
+            const newBeach = {
+              lat: 'invalid_string',
+              lng: 151.289824,
+              name: 'Manly',
+              position: 'E',
+            };
+            const response = await global.testRequest.post('/beaches').send(newBeach);
+      
+            expect(response.status).toBe(422);
+            expect(response.body).toEqual({
+              error:
+                'Beach validation failed: lat: Cast to Number failed for value "invalid_string" (type string) at path "lat"',
+            });
+          });
+      
+          it.skip('should return 500 when there is any error other than validation error', async () => {
+            //TODO think in a way to throw a 500
+          });
     });
 });
